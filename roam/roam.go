ca0d9ecad4de8f5f89de51066d7d4cc517bf3428
Jacob Wilson <jacobtwilson7@gmail.com>
2026-03-14 22:23:19 -0700

    cleanup

--- roam/roam.go
+++ roam/roam.go
@@ -11,49 +11,6 @@ import (
 	"github.com/jwil007/roamctl/wpac"
 )
 
-func Autoroam(c *wpac.Client, ctx context.Context) error {
-	//retrive current wpa_supplicant configuration
-	config, err := c.GetConfig()
-	if err != nil {
-		return fmt.Errorf("c.GetConfig: %v", err)
-	}
-	//shut off bgscan
-	bgscanOffConfig := wpac.WPAConfig{
-		SSID:      config.SSID,
-		NetworkID: config.NetworkID,
-		BGScan:    "",
-	}
-	errBG := c.SetConfig(bgscanOffConfig)
-	if errBG != nil {
-		return fmt.Errorf("c.SetConfig: %w", errBG)
-	}
-	//run scan
-	aps, errScan := c.Scan(ctx, config.SSID)
-	if errScan != nil {
-		return fmt.Errorf("wpac.Scan: %v", errScan)
-	}
-	//roam through all bssids in scan data
-	fmt.Printf("\nRoaming to %v BSSIDs\n", len(aps))
-	for _, bss := range aps {
-		result, errRoam := c.Roam(ctx, bss.BSSID)
-		if errRoam != nil {
-			log.Printf("error roaming to BSSID %v: %v", bss.BSSID, errRoam)
-		}
-		fmt.Printf("Success:%v TargetBSSID:%v FinalBSSID:%v Duration:%v Message:%v\n",
-			result.Success,
-			result.TargetBSSID,
-			result.FinalBSSID,
-			result.Duration,
-			result.Message)
-	}
-	//restore original bgscan config
-	errRes := c.SetConfig(config)
-	if errRes != nil {
-		return fmt.Errorf("c.SetConfig: %w", errRes)
-	}
-	return nil
-}
-
 func ProcessLoop(c *wpac.Client, ctx context.Context, thr Thresholds) error {
 	fmt.Println("Starting roamctl... exit with ctrl+c")
 	//Get Current wpa_supplicant status
